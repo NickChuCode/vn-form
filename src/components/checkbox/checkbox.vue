67dab3c93eda80aa29f1224b11588a8bc82da50d
NickChuCode <nickchu15@outlook.com>
2019-01-02 11:43:24 +0800

    add notes in checkbox

--- src/components/checkbox/checkbox.vue
+++ src/components/checkbox/checkbox.vue
@@ -32,10 +32,17 @@ export default {
     label: {
       type: [String, Number, Boolean]
     },
+    // 因为要在 Checkbox 组件上直接使用 v-model 来双向绑定数据，
+    // 那必不可少的一个 prop 就是 value，还有 event input，因为 v-model 本质上是一个语法糖
     value: {
       type: [String, Number, Boolean],
       default: false
     },
+    // 理论上，我们只需要给 value 设置为布尔值即可，也就是 true / false，
+    // 不过为了扩展性，我们再定义两个 props：trueValue 和 falseValue，
+    // 它们允许用户指定 value 用什么值来判断是否选中。
+    // 因为实际开发中，数据库中并不直接保存 true / false，而是 1 / 0 或其它字符串，
+    // 如果强制使用 Boolean，使用者就要再额外转换一次，这样的 API 设计不太友好。
     trueValue: {
       type: [String, Number, Boolean],
       default: true
