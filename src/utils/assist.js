ce53113e69cc7de71597d73f8253c5590cb4039e
NickChuCode <nickchu15@outlook.com>
2019-01-10 15:42:53 +0800

    add deepCopy in utils

--- src/utils/assist.js
+++ src/utils/assist.js
@@ -40,6 +40,35 @@ function findComponentsDownward (context, componentName) {
   }, [])
 }
 
+function typeOf (obj) {
+  return Object.prototype.toString.call(obj).slice(8, -1).toLowerCase()
+}
+
+function deepCopy (data) {
+  const t = typeOf(data)
+  let o
+
+  if (t === 'array') {
+    o = []
+  } else if (t === 'object') {
+    o = {}
+  } else {
+    return data
+  }
+
+  if (t === 'array') {
+    for (let i = 0; i < data.length; i++) {
+      o.push(deepCopy(data[i]))
+    }
+  } else if (t === 'object') {
+    for (let i in data) {
+      o[i] = deepCopy(data[i])
+    }
+  }
+  return o
+}
+
 export { findComponentUpward }
 export { findComponentDownward }
 export { findComponentsDownward }
+export { deepCopy }
